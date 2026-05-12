b711f692e5dc6f5b0cfc653091a6190721de29ff
Jadson Serafim <jadsonSG57@gmail.com>
2026-05-12 18:16:44 -0300

    Adiciona Redis e PostgreSql na aplicação, cria o AppDbContext e o BaseRepository

--- UrlShorter.Infrastructure/DependencyInjection.cs
+++ UrlShorter.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,8 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using UrlShorter.Domain.Interfaces;
+using UrlShorter.Infrastructure.Persistence;
 
 namespace UrlShorter.Infrastructure;
 
@@ -7,13 +10,20 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        // TODO: registrar DbContext
-        // services.AddDbContext<AppDbContext>(options =>
-        //     options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+        
+        services.AddStackExchangeRedisCache(options =>
+        {
+            options.Configuration = configuration.GetConnectionString("Redis");
+            options.InstanceName = "UrlShorter";
+        });
 
         // TODO: registrar Repositórios
         // services.AddScoped<IExampleRepository, ExampleRepository>();
-
+        
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        
         return services;
     }
 }
