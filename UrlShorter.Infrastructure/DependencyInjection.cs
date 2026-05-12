5f5473331729d87c677741b2be48b627714ffe0d
Jadson Serafim <jadsonSG57@gmail.com>
2026-05-12 19:34:18 -0300

    Revert "Adiciona Redis e PostgreSql na aplicação, cria o AppDbContext e o BaseRepository"
    This reverts commit 650711e21cd63eea566bcc319193c496e9a6907c.

--- UrlShorter.Infrastructure/DependencyInjection.cs
+++ UrlShorter.Infrastructure/DependencyInjection.cs
@@ -1,8 +1,5 @@
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
-using UrlShorter.Domain.Interfaces;
-using UrlShorter.Infrastructure.Persistence;
 
 namespace UrlShorter.Infrastructure;
 
@@ -10,20 +7,13 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<AppDbContext>(options =>
-            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
-        
-        services.AddStackExchangeRedisCache(options =>
-        {
-            options.Configuration = configuration.GetConnectionString("Redis");
-            options.InstanceName = "UrlShorter";
-        });
+        // TODO: registrar DbContext
+        // services.AddDbContext<AppDbContext>(options =>
+        //     options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
 
         // TODO: registrar Repositórios
         // services.AddScoped<IExampleRepository, ExampleRepository>();
-        
-        services.AddScoped<IUnitOfWork, UnitOfWork>();
-        
+
         return services;
     }
 }
