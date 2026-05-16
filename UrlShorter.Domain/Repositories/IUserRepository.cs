3aa8cf6c58791ba18a1235dbcda1b7da5f1b68a0
Jadson Serafim <jadsonSG57@gmail.com>
2026-05-15 22:01:34 -0300

    Criada a interface IUserRepository, aproveitando o IBaseRepository criado anteriormente

---
+++ UrlShorter.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+using UrlShorter.Domain.Entities;
+
+namespace UrlShorter.Domain.Interfaces;
+
+public interface IUserRepository : IBaseRepository<User>
+{
+    Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
+    Task<bool> ExistsAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<bool> IsEmailUniqueAsync(string email, CancellationToken cancellationToken = default);
+
+}
