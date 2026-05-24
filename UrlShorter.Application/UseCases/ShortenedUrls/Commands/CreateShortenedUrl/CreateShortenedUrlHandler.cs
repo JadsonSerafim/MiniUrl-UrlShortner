35aad45dd3000d94978460b6ba7119663705ae3d
Jadson Serafim <jadsonSG57@gmail.com>
2026-05-23 22:10:09 -0300

    Adiciona limites de urls para usuarios e regra para guests

--- UrlShorter.Application/UseCases/ShortenedUrls/Commands/CreateShortenedUrl/CreateShortenedUrlHandler.cs
+++ UrlShorter.Application/UseCases/ShortenedUrls/Commands/CreateShortenedUrl/CreateShortenedUrlHandler.cs
@@ -23,7 +23,23 @@ public class CreateShortenedUrlHandler : IRequestHandler<CreateShortenedUrlComma
 
     public async Task<Result<string>> Handle(CreateShortenedUrlCommand request, CancellationToken cancellationToken)
     {
-         int tentativas_maxima = 5;
+
+        if (request.UserId == null)
+        {
+            var existingUrl = await _shortenedUrlRepository.GetActiveGuestUrlAsync(request.OriginalUrl, cancellationToken);
+            if (existingUrl != null)
+            {
+                return Result<string>.Success(existingUrl.ShortCode);
+            }
+        }
+
+        int activeCount = 0;
+        if (request.UserId != null)
+        {
+            activeCount = await _shortenedUrlRepository.CountActiveByUserIdAsync(request.UserId.Value, cancellationToken);
+        }
+
+        int maxTries = 5;
         var urlResult = Url.Create(request.OriginalUrl);
         if (urlResult.IsFailure)
         {
@@ -31,20 +47,19 @@ public class CreateShortenedUrlHandler : IRequestHandler<CreateShortenedUrlComma
         }
 
         string shortCode = _shortCodeGenerator.Generate();
-        
         while(await _shortenedUrlRepository.ShortCodeExistsAsync(shortCode, cancellationToken))
         {
-            if(tentativas_maxima == 0)
+            if(maxTries == 0)
             {
                 return ErrorsUrl.ShortCodeAlreadyExists;
             }
 
             shortCode = _shortCodeGenerator.Generate();
 
-            tentativas_maxima--;
+            maxTries--;
         }
 
-        var shortenedUrlResult = ShortenedUrl.Create(urlResult.Value, shortCode, request.UserId, request.ExpiresAt);
+        var shortenedUrlResult = ShortenedUrl.Create(urlResult.Value, shortCode, request.UserId, request.ExpiresAt, activeCount);
         if(shortenedUrlResult.IsFailure) return Result<string>.Failure(shortenedUrlResult.Error);
 
         await _shortenedUrlRepository.AddAsync(shortenedUrlResult.Value, cancellationToken);
